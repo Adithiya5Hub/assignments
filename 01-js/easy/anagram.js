38b3d4a66c42722edc5f1a03171c45bb403c6524
S. Adithiya <93365795+Adithiya5Hub@users.noreply.github.com>
2023-12-04 20:52:40 +0530

    Update anagram.js

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -5,7 +5,20 @@
 */
 
 function isAnagram(str1, str2) {
-
+  let n1 = str1.length;
+    let n2 = str2.length;
+    if(n1!==n2){
+        console.log("Invalid Input");
+       
+    }
+   const string1 = str1.split('').sort().join('');
+   const string2 = str2.split('').sort().join('');
+   return string1 === string2;
 }
-
+var answer = isAnagram("rasp","spare");
+if (answer) {
+    console.log("They're anagrams");
+} else {
+    console.log("They're not anagrams");
+}
 module.exports = isAnagram;
